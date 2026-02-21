a749f458fc556c8889b2af0a1650530c771b0842
Anushtha-Rathore <anushtharathore35@gmail.com>
2026-02-21 16:16:06 +0530

    chore: corrected

--- config/authApi.js
+++ config/authApi.js
@@ -6,16 +6,11 @@ const PROXY_URL = process.env.NEXT_PUBLIC_PROXY_URL;
 const PYTHON_URL = process.env.NEXT_PUBLIC_PYTHON_SERVER_URL;
 
 // User Authentication APIs
-export const userdetails = async ({ exclude_role_ids = process.env.PROXY_USER_ROLE_ID, role_ids } = {}) => {
+export const userdetails = async ({ exclude_role_ids = process.env.NEXT_PUBLIC_PROXY_USER_ROLE_ID, role_ids } = {}) => {
   try {
-    let url = `${PROXY_URL}/api/c/getDetails`;
-    const params = new URLSearchParams();
-    if (exclude_role_ids) params.append('exclude_role_ids', exclude_role_ids);
-    if (role_ids) params.append('role_ids', role_ids);
-    if (params.toString()) {
-      url += `?${params.toString()}`;
-    }
-    const details = await axios.get(url);
+    const details = await axios.get(`${PROXY_URL}/api/c/getDetails`, {
+      params: { exclude_role_ids, role_ids },
+    });
     return details;
   } catch (error) {
     console.error(error);
